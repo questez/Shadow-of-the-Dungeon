116624a5ccea2357748c194cb644c6b21498f2ac
IaroslavKuzichev <iaroslavkuzichev@gmail.com>
2025-05-05 09:51:37 +0300

    Мелкие доработки мечей
    Начата работа над функцией покупки мечей

--- Shadow of the Dungeon/Assets/Scripts/UI/BuySword.cs
+++ Shadow of the Dungeon/Assets/Scripts/UI/BuySword.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class BuySword : MonoBehaviour
 {
     [SerializeField] UnityEngine.UI.Button _button;
+    [SerializeField] MonoBehaviour GrabInteractible;
     public void Buy()
     {
         Debug.Log("Bought a new sword");
         _button.gameObject.SetActive(false);
+        GrabInteractible.enabled = true;
     }
     private void Awake()
     {
         _button.onClick.AddListener(Buy);
+        GrabInteractible.enabled = false;
     }
 }
