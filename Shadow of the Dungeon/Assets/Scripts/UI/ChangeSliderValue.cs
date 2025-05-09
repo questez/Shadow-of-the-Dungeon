cf7ca728fcfea29b622f58542311d0a977b0e4f6
IaroslavKuzichev <iaroslavkuzichev@gmail.com>
2025-05-09 18:34:41 +0300

    Доработка HUD, покупки предметов и сбора монет
    Доработаны HUD, покупка предметов и сбор монет. Теперь сердечки убавляются при уроне, а при покупке предмета проверяется баланс и уменьшается на стоимость предмета. Уменьшение баланса отражается в HUD.
    Начата работа над экраном паузы

--- Shadow of the Dungeon/Assets/Scripts/UI/ChangeSliderValue.cs
+++ Shadow of the Dungeon/Assets/Scripts/UI/ChangeSliderValue.cs
@@ -1,11 +1,22 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangeSliderValue : MonoBehaviour
 {
+    [SerializeField] TMP_Text _text;
     [SerializeField] Slider _slider;
+    [SerializeField] AudioSource _musicSource;
     public void ChangeValue()
     {
-        Debug.Log("Volume: " + _slider.value);
+        if (_text.text == "Sound Volume")
+        {
+            Debug.Log("Sound Volume: " + _slider.value);
+        }
+        else
+        {
+            Debug.Log("Music Volume: " + _slider.value);
+            _musicSource.volume = _slider.value / 100;
+        }
     }
 }
